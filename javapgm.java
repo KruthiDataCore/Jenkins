f6ddd0b7518df835f535472399d55f3a9e8d1754
KruthiDataCore <139320943+KruthiDataCore@users.noreply.github.com>
2023-08-02 11:14:24 +0530

    Update javapgm.java

--- javapgm.java
+++ javapgm.java
@@ -1,5 +1,19 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'z';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+    }
+}
